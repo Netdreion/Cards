333732131f004b6de1e7513a9c7d097ae697eb5b
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-24 22:34:53 -0400

    Array created

---
+++ src/data.jsx
@@ -0,0 +1,57 @@
+const catArray = [
+  {
+    name: "Whiskers",
+    breed: "Siamese",
+    age: 3,
+    img: "https://example.com/whiskers.jpg",
+    url: "https://example.com/whiskers-page",
+    description:
+      "Whiskers is a friendly Siamese cat with striking blue eyes and a playful personality.",
+  },
+  {
+    name: "Mittens",
+    breed: "Maine Coon",
+    age: 5,
+    img: "https://example.com/mittens.jpg",
+    url: "https://example.com/mittens-page",
+    description:
+      "Mittens is a fluffy Maine Coon known for their large size and affectionate nature.",
+  },
+  {
+    name: "Oscar",
+    breed: "Persian",
+    age: 2,
+    img: "https://example.com/oscar.jpg",
+    url: "https://example.com/oscar-page",
+    description:
+      "Oscar is a luxurious Persian cat with long, elegant fur and a calm demeanor.",
+  },
+  {
+    name: "Luna",
+    breed: "Sphynx",
+    age: 1,
+    img: "https://example.com/luna.jpg",
+    url: "https://example.com/luna-page",
+    description:
+      "Luna is a unique Sphynx cat known for their lack of fur and affectionate personality.",
+  },
+  {
+    name: "Simba",
+    breed: "Bengal",
+    age: 2,
+    img: "https://example.com/simba.jpg",
+    url: "https://example.com/simba-page",
+    description:
+      "Simba is a stunning Bengal cat with distinctive markings and a playful nature.",
+  },
+  {
+    name: "Charlie",
+    breed: "Scottish Fold",
+    age: 4,
+    img: "https://example.com/charlie.jpg",
+    url: "https://example.com/charlie-page",
+    description:
+      "Charlie is an adorable Scottish Fold cat known for their folded ears and gentle disposition.",
+  },
+];
+export default catArray;
